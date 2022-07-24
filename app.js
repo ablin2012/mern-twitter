5d9ac97edf28325a2739a35b06ab7743bb91fc43
ablin2012 <adamlin.2009@gmail.com>
2022-07-23 23:24:28 -0700

    Create user auth

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("sup pussy"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 
