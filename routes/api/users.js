934407ef3ffb88a71ca852c216d58a9d4a048e4e
ablin2012 <adamlin.2009@gmail.com>
2022-07-24 22:35:04 -0700

    Finished MERN Twitter

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,10 +9,6 @@ const passport = require('passport');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-router.get('/test', (req, res) => {
-    res.json({msg: 'this is the user route'});
-});
-
 router.post("/register", (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
   
